ec36b3f38821b62c9ee8c220660f77c98a5d4527
KJ Jenkins <99845714+KJinkies@users.noreply.github.com>
2022-03-21 08:55:39 +1300

    NEW repository
    Excludes node modules - to re-install, in terminal type:
    
    npm install

---
+++ new-vue-practice-team-a/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import KjComponent from "./components/kj-component.vue";
+import MustafaComponent from "./components/mustafa-component.vue";
+import SeanComponent from "./components/sean-component.vue";
+import SunnyComponent from "./components/sunny-component.vue";
+import MaxComponent from "./components/max-component.vue"; 
+</script>
+
+<template>
+
+<MustafaComponent/>
+
+<SeanComponent/>
+
+<SunnyComponent/>
+
+<MaxComponent/>
+  
+<KjComponent/>
+
+  
+</template>
+
+<style>
+
+</style>
