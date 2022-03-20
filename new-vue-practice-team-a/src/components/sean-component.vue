f9981fe00b17389d38c1e1819e81790b9b6b48ab
KJ Jenkins <99845714+KJinkies@users.noreply.github.com>
2022-03-21 09:03:18 +1300

    added sunny and sean components

---
+++ new-vue-practice-team-a/src/components/sean-component.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>This is your component sean please edit and style</h1>
+</template>
+
+<style>
+
+</style>
+
+<script>
+
+</script>
---
+++ new-vue-practice-team-a/src/components/sunny-component.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>This is your component please edit and style</h1>
+</template>
+
+<style>
+
+</style>
+
+<script>
+
+</script>
